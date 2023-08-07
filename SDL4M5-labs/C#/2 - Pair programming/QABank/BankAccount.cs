3b0329fcfc08573121eb8bcffe789fb62648a0cd
Nathan O'Brien <nathan.ndmarketing@gmail.com>
2023-08-07 16:48:14 +0100

    Initial Commit

---
+++ SDL4M5-labs/C#/2 - Pair programming/QABank/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QABank
+{
+    public class BankAccount
+    {
+        public string CustomerName { get; set; }
+        public int? AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+
+        public static int newAccountNumber = 100000;
+        public BankAccount(string customerName, decimal balance, int? accountNumber = null)
+        {
+            
+            CustomerName = customerName;
+
+            if(accountNumber == null) 
+            {
+                AccountNumber = newAccountNumber; 
+                GenerateAccountNumber();
+            }
+
+            else
+            {
+                AccountNumber = accountNumber;
+            }
+            Balance = balance;
+        }
+
+        public int GenerateAccountNumber()
+        {
+            return newAccountNumber++;
+        }
+
+        public void Deposit (decimal balance, decimal depositAmount)
+        {
+            Balance = balance + depositAmount;
+        }
+
+        public void Withdraw(decimal balance, decimal withdrawAmount)
+        {
+            Balance = balance - withdrawAmount;
+        }
+    }
+}
---
+++ SDL4M5-labs/C#/2 - Pair programming/QABank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace QABank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount("Jimmy",30.5m);
+            BankAccount bankAccount2 = new BankAccount("Pete", 30.5m);
+
+            Console.WriteLine(bankAccount.AccountNumber);
+            Console.WriteLine(bankAccount2.AccountNumber);
+
+            bankAccount.Deposit(bankAccount.Balance, 100m);
+            Console.WriteLine(bankAccount.Balance);
+
+            Console.ReadLine();
+
+        }
+    }
+}
