2f4027fe838ac4ad909100972fcbfaa5db7baa2d
Nathan O'Brien <nathan.ndmarketing@gmail.com>
2023-08-08 09:03:00 +0100

    Scaffolded out Current and Savings Account

---
+++ SDL4M5-labs/C#/2 - Pair programming/QABank/Services/CurrentAccountService.cs
@@ -0,0 +1,23 @@
+﻿using QABank.Interfaces;
+using System;
+
+namespace QABank.Services
+{
+    public class CurrentAccountService : IBankAccountService
+    {
+        public decimal Deposit(decimal depositAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GenerateAccountNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Withdraw(decimal depositAmount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SDL4M5-labs/C#/2 - Pair programming/QABank/Services/SavingsAccountService.cs
@@ -0,0 +1,27 @@
+﻿using QABank.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QABank.Services
+{
+    public class SavingsAccountService : IBankAccountService
+    {
+        public decimal Deposit(decimal depositAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GenerateAccountNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Withdraw(decimal depositAmount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
