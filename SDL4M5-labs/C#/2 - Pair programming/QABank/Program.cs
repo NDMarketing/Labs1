38921df3ea6e1e8270908a00c0b118c216a7ee76
Nathan O'Brien <nathan.ndmarketing@gmail.com>
2023-08-08 08:52:08 +0100

    Amended spacing

--- SDL4M5-labs/C#/2 - Pair programming/QABank/Program.cs
+++ SDL4M5-labs/C#/2 - Pair programming/QABank/Program.cs
@@ -6,7 +6,7 @@ namespace QABank
     class Program
     {
         // helper for printing results
-        static void printResults (string transaction, BankAccountService account)
+        static void printResults(string transaction, BankAccountService account)
         {
             Console.WriteLine($"Customer Name: {account.CustomerName}, Account Number: {account.AccountNumber}, Balance: {account.Balance}");
             if (transaction != null)
@@ -21,12 +21,12 @@ namespace QABank
         {
             // creating bankAccount Objects and add to list
             List<BankAccountService> accounts = new List<BankAccountService>();
-            accounts.Add( new BankAccountService("Jimmy",30.5m));
+            accounts.Add(new BankAccountService("Jimmy", 30.5m));
             accounts.Add(new BankAccountService("Pete", 30.5m, 100002));
             accounts.Add(new BankAccountService("Paul", 30.5m));
 
             // run through accounts and print details.
-            foreach (BankAccountService account in accounts) 
+            foreach (BankAccountService account in accounts)
             {
                 Console.WriteLine("Starting Balance**************************"); printResults(null, account);
                 account.Deposit(100); printResults("Deposit", account);
