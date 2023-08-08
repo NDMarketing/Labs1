8e0c2d077578787ad08d6a4bac25f99e5ce40273
Nathan O'Brien <nathan.ndmarketing@gmail.com>
2023-08-08 08:46:37 +0100

    Removed from wrong location

--- SDL4M5-labs/C#/2 - Pair programming/QABank/Interfaces/IBankAccountService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QABank.Interfaces
-{
-    public interface IBankAccountService
-    {
-        int GenerateAccountNumber();
-        decimal Deposit(decimal depositAmount);
-        decimal Withdraw(decimal depositAmount);
-    }
-}
